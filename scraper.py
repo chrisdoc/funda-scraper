5b4fe33b29ec99f970f340dbaa6c91dc4874bb09
Christoph Kieslich <c.kieslich@gmail.com>
2024-03-30 18:00:05 +0100

    Add ruff library and update TelegramBot class

--- scraper.py
+++ scraper.py
@@ -2,25 +2,33 @@ from funda import Funda
 from telegram_bot import TelegramBot
 import os
 import sys
+import re
+
 
 MESSAGE_TEMPLATE = """
-New apartment is out\.
-The elevator is *%s*\.
+New apartment is out
+The elevator is *%s*
 
-Number of stories \- *%s*\.
-Listing price \- *%s*\.
+Number of stories  *%s*
+Listing price  *%s*
 
 [check out on funda](%s)
 """
-ENV_FUNDA_DB = 'FUNDA_DB'
-ENV_TELEGRAM_TOKEN = 'TELEGRAM_TOKEN'
-ENV_CHAT_ID = 'CHAT_ID'
-ENV_SEARCH_URL = 'SEARCH_URL'
 
-if ENV_FUNDA_DB not in os.environ or ENV_TELEGRAM_TOKEN not in os.environ \
-    or ENV_CHAT_ID not in os.environ:
-    print(f'ERROR: The following environment variables must be set: {ENV_FUNDA_DB}, {ENV_TELEGRAM_TOKEN}, {ENV_CHAT_ID} and {ENV_SEARCH_URL}',
-           file=sys.stderr)
+ENV_FUNDA_DB = "FUNDA_DB"
+ENV_TELEGRAM_TOKEN = "TELEGRAM_TOKEN"
+ENV_CHAT_ID = "CHAT_ID"
+ENV_SEARCH_URL = "SEARCH_URL"
+
+if (
+    ENV_FUNDA_DB not in os.environ
+    or ENV_TELEGRAM_TOKEN not in os.environ
+    or ENV_CHAT_ID not in os.environ
+):
+    print(
+        f"ERROR: The following environment variables must be set: {ENV_FUNDA_DB}, {ENV_TELEGRAM_TOKEN}, {ENV_CHAT_ID} and {ENV_SEARCH_URL}",
+        file=sys.stderr,
+    )
     exit(1)
 
 sqlite_location = os.environ[ENV_FUNDA_DB]
@@ -33,9 +41,13 @@ telegramBot = TelegramBot(chat_id, telegram_token)
 
 newListing = funda.fetchNew()
 for listing in newListing:
-    print('scraper: sending message over telegram')
-    telegramBot.postMessage(MESSAGE_TEMPLATE % 
-        ('mentioned' if listing.hasElevator else 'not mentioned',
-         listing.stories,
-         listing.price.replace('.', '\\.'),
-         listing.link))
+    print("scraper: sending message over telegram")
+    telegramBot.postMessage(
+        MESSAGE_TEMPLATE
+        % (
+            "mentioned" if listing.hasElevator else "not mentioned",
+            re.sub(r"[^0-9.]", "", listing.stories),
+            listing.price.replace(".", "\\.").replace("k\\.k\\.", ""),
+            listing.link,
+        )
+    )
